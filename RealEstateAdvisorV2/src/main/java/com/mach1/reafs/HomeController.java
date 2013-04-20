b4483fed4032f8d6a245b37a11b404a8d57bae2a
Uku-Rasmus <ukurasmus@gmail.com>
2013-04-20 23:45:34 +0300

    Removed Drools dependencies.

--- RealEstateAdvisorV2/src/main/java/com/mach1/reafs/HomeController.java
+++ RealEstateAdvisorV2/src/main/java/com/mach1/reafs/HomeController.java
@@ -5,7 +5,6 @@ import java.util.Locale;
 
 import jess.JessException;
 
-import org.drools.runtime.StatefulKnowledgeSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
